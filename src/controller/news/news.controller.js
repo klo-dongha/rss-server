b40cc20a7f054a06e02c532a126db36ca4566ef9
godongha <godongha@jinhakapply.com>
2022-07-11 14:31:08 +0900

    rss 샘플 파라미터 변경 및 신문사별 샘플 데이터 추출 API 추가

--- src/controller/news/news.controller.js
+++ src/controller/news/news.controller.js
@@ -1,17 +1,39 @@
 import RssService from '@/service/news/rss.service';
-
 import express from 'express';
+
 const router = express.Router();
 
 router.get('/sample-rss', async (req, res, next) => {
+  const rssUrl = req.query.rssUrl;
+
+  console.log('rssUrl::', rssUrl);
+
   const rssService = new RssService();
-  const result = await rssService.getSampleRssOne();
+  const result = await rssService.getSampleRssOne(rssUrl);
   res.status(200).json(result);
 });
 
+router.get('/sample-rss-copyright', async (req, res, next) => {
+  const copyright = req.query.copyright;
+
+  console.log('copyright::', copyright);
+
+  if (copyright) {
+    const rssService = new RssService();
+    const result = await rssService.getSampleRssCopyright(copyright);
+    res.status(200).json(result);
+  } else {
+    res.status(200).json(null);
+  }
+});
+
 router.get('/sample-scrapping', async (req, res, next) => {
+  const scrappingUrl = req.query.scrappingUrl;
+
+  console.log('scrappingUrl::', scrappingUrl);
+
   const rssService = new RssService();
-  const result = await rssService.getSampleScrapping();
+  const result = await rssService.getSampleScrapping(scrappingUrl);
   res.status(200).json(result);
 });
 
--- src/service/news/rss.service.js
+++ src/service/news/rss.service.js
@@ -2,19 +2,77 @@ import rssSummaryObj from './news.summary.rss.json';
 import rssFullObj from './news.full.rss.json';
 import { parse } from 'rss-to-json';
 import Elasticsearch from '@/elasticsearch/elasticsearch';
-import moment from 'moment';
 import puppeteer from 'puppeteer';
+import dayjs from 'dayjs';
 
 export default class RssService {
-  async getSampleRssOne() {
-    const rssResult = await parse(
-      'https://www.fnnews.com/rss/r20/fn_realnews_politics.xml'
-    );
-    console.log('rssResult', rssResult.items[0]);
-    return rssResult;
+  /**
+   * 기사 단위의 RSS 샘플
+   * @param {*} rssUrl - rss 도메인 주소
+   * @return {*} - rss 결과
+   * @memberof RssService
+   */
+  async getSampleRssOne(rssUrl) {
+    try {
+      const rssResult = await parse(rssUrl);
+      return rssResult;
+    } catch (e) {
+      console.log('e', e);
+      throw e;
+    }
   }
 
-  async getSampleScrapping() {
+  /**
+   * 신문사 단위의 RSS 샘플
+   * @param {*} copyright - 회사명(한글)
+   * @return {*} - rss 결과
+   * @memberof RssService
+   */
+  async getSampleRssCopyright(copyright, type = 'summary') {
+    const rssList = rssSummaryObj;
+    const rssResultArr = [];
+    for (const k2 in rssList[copyright]) {
+      const rssAddress = rssList[copyright][k2];
+      const rssResult = await parse(rssAddress);
+      console.log('rssAddress', k2, rssAddress);
+      const rssResultItem = rssResult.items;
+      for (const item of rssResultItem) {
+        try {
+          // set body
+          const data = {};
+          data.copyright = copyright;
+          data.category = k2;
+          data.title = item.title;
+          data.link = item.link;
+          data.description_summary = item.description
+            ? item.description.substring(0, 100) + '...'
+            : null;
+          data.description = type === 'summary' ? null : item.description;
+          data.created = item.created
+            ? dayjs(item.created).format('YYYY-MM-DD HH:mm:ss')
+            : null;
+          data.published = item.published
+            ? dayjs(item.published).format('YYYY-MM-DD HH:mm:ss')
+            : null;
+          data.enclosures = item.enclosures ? item.enclosures : null;
+          data.media = item.enclosures ? item.media : null;
+
+          rssResultArr.push(data);
+        } catch (e) {
+          console.log('e', e);
+        }
+      }
+    }
+    return { rssCount: rssResultArr.length, rssArr: rssResultArr };
+  }
+
+  /**
+   * 기사 단위의 스크래핑 샘플
+   * @param {*} scrappingUrl
+   * @return {*}
+   * @memberof RssService
+   */
+  async getSampleScrapping(scrappingUrl) {
     // await (async () => {
     //   const browser = await puppeteer.launch();
     //   const page = await browser.newPage();
@@ -26,9 +84,7 @@ export default class RssService {
     console.log('getSampleScrapping in!');
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.goto(
-      'https://news.jtbc.joins.com/article/article.aspx?news_id=NB12026856'
-    );
+    await page.goto(scrappingUrl);
 
     const articlebody = await page.$('#articlebody > div:nth-child(1)');
     if (!articlebody) {
@@ -45,49 +101,68 @@ export default class RssService {
     return constents;
   }
 
+  /**
+   * 전체 신문사의 rss 데이터를 저장하며, 해당 기사의 url에서 스크래핑하여 기사를 저장
+   *
+   * @memberof RssService
+   */
   async setRss() {
     // summary ver
     await this.insertRssData(rssSummaryObj, 'summary');
     // full ver
-    await this.insertRssData(rssFullObj, 'full');
+    // await this.insertRssData(rssFullObj, 'full');
   }
 
-  /** elasticsearch insert */
+  /**
+   * rss에서 데이터를 받아와 elasticsearch에 저장
+   * @param {*} rssList - 전체 신문사 rss 도메인 정보(요약본 기사 버전과 전체 기사 버전으로 분리 호출)
+   * @param {string} [type='summary'] - summary: 요약 기사 버전, full: 전체 기사 버전 [description에 데이터를 넣을지 말지를 정의]
+   * @memberof RssService
+   */
   async insertRssData(rssList, type = 'summary') {
     const elasticsearch = new Elasticsearch();
     for (const k1 in rssList) {
+      console.log(k1 + ' start !');
       for (const k2 in rssList[k1]) {
-        const rssAddress = rssList[k1][k2];
-        const rssResult = await parse(rssAddress);
-        const rssResultItem = rssResult.items;
-        for (const item of rssResultItem) {
-          // set body
-          const data = {};
-          data.copyright = k1;
-          data.category = k2;
-          data.title = item.title;
-          data.link = item.link;
-          data.description_summary = item.description
-            ? item.description.substring(0, 100) + '...'
-            : null;
-          data.description = type === 'summary' ? null : item.description;
-          data.created = item.created
-            ? moment(item.created).format('YYYY-MM-DD HH:mm:ss')
-            : null;
-          data.published = item.published
-            ? moment(item.published).format('YYYY-MM-DD HH:mm:ss')
-            : null;
-          data.enclosures = item.enclosures ? item.enclosures : null;
-          data.media = item.enclosures ? item.media : null;
+        try {
+          const rssAddress = rssList[k1][k2];
+          console.log('rssAddress', k2, rssAddress);
+          const rssResult = await parse(rssAddress);
+          const rssResultItem = rssResult.items;
 
-          // insert data
-          await elasticsearch.index({
-            index: 'news',
-            id: data.link,
-            body: data,
-          });
+          for (const item of rssResultItem) {
+            // set body
+            const data = {};
+            data.copyright = k1;
+            data.category = k2;
+            data.title = item.title;
+            data.link = item.link;
+            data.description_summary = item.description
+              ? item.description.substring(0, 100) + '...'
+              : null;
+            data.description = type === 'summary' ? null : item.description;
+            data.created = item.created
+              ? dayjs(item.created).format('YYYY-MM-DD HH:mm:ss')
+              : null;
+            data.published = item.published
+              ? dayjs(item.published).format('YYYY-MM-DD HH:mm:ss')
+              : null;
+            data.enclosures = item.enclosures ? item.enclosures : null;
+            data.media = item.enclosures ? item.media : null;
+
+            // insert data
+            await elasticsearch.index({
+              index: 'news',
+              id: data.link,
+              body: data,
+            });
+          }
+        } catch (e) {
+          console.log('e', e);
+          continue;
         }
       }
+      console.log(k1 + ' end !');
     }
     console.log(type + ' end!');
   }
